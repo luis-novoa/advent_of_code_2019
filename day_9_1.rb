17ea8d936173bf1d06d78e911f91eb6635e6a0de
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-20 19:08:37 -0400

    Codes most of the changes required

--- day_9_1.rb
+++ day_9_1.rb
@@ -6,14 +6,16 @@ class Computer
     @array = array
   end
 
-  def program(input1, input2)
+  def program(*input)
     @used_input = 0 if @used_input.nil?
     @i = 0 if @i.nil?
+    @relative_base = 0 if @relative_base.nil?
     @break = "no"
     loop do
+      # p @i
       modes(@array[@i])
       operation(@array[@i])
-      execute(@modes, input1, input2)
+      execute(@modes, input)
       if @break == "yes"
         return @output
       end
@@ -39,41 +41,64 @@ class Computer
     @modes[2] ||= 0
     j = 1
     @modes.map! do |e|
-      @array[@i + j] ||= 0
-      if e.to_i.zero?
-        e = @array[@array[@i + j]]
-      else
+      selected = 0
+      case e
+      when "1"
         e = @array[@i + j]
+
+      when "2"
+        selected = rotator(@relative_base + @array[@i + j])
+        e = @array[selected]
+
+      else
+        selected = rotator(@array[@i + j])
+        e = @array[selected]
       end
       j += 1
       e
     end
   end
 
-  def execute(array_of_modes, pass_input1, pass_input2)
+  def rotator(index)
+    new_index = index
+    length = @array.length.clone
+    loop do
+      p length if new_index < 0 #Note: this calculates negative numbers, but it shouldn't.
+      break if new_index < length
+      # p 'what' if new_index < 0
+      new_index -= length
+    end
+    new_index
+  end
+
+  def execute(array_of_modes, input)
+    selected_3 = rotator(@array[@i + 3])
+    # p @array
+    # p @array[@i]
+    # p array_of_modes
+    @break = 'yes' if @array[4] == 218
     case @op
     when 1
-      @array[@array[@i + 3]] = array_of_modes[0] + array_of_modes[1]
+      @array[selected_3] = array_of_modes[0] + array_of_modes[1]
       @i += 4
-
+      # @break = 'yes'
 
     when 2
-      @array[@array[@i + 3]] = array_of_modes[0] * array_of_modes[1]
+      @array[selected_3] = array_of_modes[0] * array_of_modes[1]
       @i += 4
 
-
     when 3
-      @array[@array[@i + 1]] = pass_input1 if @used_input == 0
-      @array[@array[@i + 1]] = pass_input2 if @used_input > 0
+      selected_1 = rotator(@array[@i + 1])
+      @array[selected_1] = input[0] if @used_input == 0
+      @array[selected_1] = input[1] if @used_input > 0
       @used_input += 1
       @i += 2
-
 
     when 4
       @i += 2
       @output = array_of_modes[0]
-      @break = "yes"
-
+      p @output
+      # @break = "yes" if @output == 204
 
     when 5
       unless array_of_modes[0].zero?
@@ -82,7 +107,6 @@ class Computer
         @i += 3
       end
 
-
     when 6
       if array_of_modes[0].zero?
         @i = array_of_modes[1]
@@ -90,57 +114,44 @@ class Computer
         @i += 3
       end
 
-
     when 7
       if array_of_modes[0] < array_of_modes[1]
-        @array[@array[@i + 3]] = 1
+        @array[selected_3] = 1
       else
-        @array[@array[@i + 3]] = 0
+        @array[selected_3] = 0
       end
       @i += 4
-
 
     when 8
       if array_of_modes[0] == array_of_modes[1]
-        @array[@array[@i + 3]] = 1
+        @array[selected_3] = 1
       else
-        @array[@array[@i + 3]] = 0
+        @array[selected_3] = 0
       end
       @i += 4
 
+    when 9
+      # p @array
+      # p @i
+      # p @array[@i]
+      # p array_of_modes
+      # @break = 'yes' if array_of_modes[0].zero?
+      @relative_base += array_of_modes[0]
+      @i += 2
+      # p @array
+
 
     when 99
-      @output = "finished"
+      # @output = "finished"
       @break = "yes"
 
     end
   end
 end
 
-settings = (5..9).to_a.permutation.to_a
-outputs = Hash.new
-amp_software = [3,8,1001,8,10,8,105,1,0,0,21,38,55,64,81,106,187,268,349,430,99999,3,9,101,2,9,9,1002,9,2,9,101,5,9,9,4,9,99,3,9,102,2,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,1002,9,5,9,1001,9,4,9,102,4,9,9,4,9,99,3,9,102,2,9,9,1001,9,5,9,102,3,9,9,1001,9,4,9,102,5,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,99]
-settings.each_with_index do |combination, index|
-  amp_a = Computer.new(amp_software.clone)
-  amp_b = Computer.new(amp_software.clone)
-  amp_c = Computer.new(amp_software.clone)
-  amp_d = Computer.new(amp_software.clone)
-  amp_e = Computer.new(amp_software.clone)
-  output = 0
-  loop do
-    last_output_a = amp_a.program(combination[0], output) unless last_output_a == "finished"
-    output = last_output_a unless last_output_a == "finished"
-    last_output_b = amp_b.program(combination[1], output) unless last_output_b == "finished"
-    output = last_output_b unless last_output_b == "finished"
-    last_output_c = amp_c.program(combination[2], output) unless last_output_c == "finished"
-    output = last_output_c unless last_output_c == "finished"
-    last_output_d = amp_d.program(combination[3], output) unless last_output_d == "finished"
-    output = last_output_d unless last_output_d == "finished"
-    last_output_e = amp_e.program(combination[4], output) unless last_output_e == "finished"
-    break if last_output_a == "finished" && last_output_b == "finished" && last_output_c == "finished" && last_output_d == "finished" && last_output_e == "finished"
-    output = last_output_e unless last_output_e == "finished"
-  end
-  outputs[output] = index
-end
-
-p outputs.max
+# input = File.read("inputs/day9.txt").chomp.split(',')
+# new_intcode = Computer.new(input)
+new_intcode = Computer.new([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+# new_intcode = Computer.new([1102,34915192,34915192,7,4,7,99,0]) #works
+# new_intcode = Computer.new([104,1125899906842624,99]) #works
+new_intcode.program
